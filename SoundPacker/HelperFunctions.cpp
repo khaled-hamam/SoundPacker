132eba39077cbab98b2d29455756b79dd223520a
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2018-04-10 12:27:51 +0200

    Implemented generateMetadata function

--- SoundPacker/HelperFunctions.cpp
+++ SoundPacker/HelperFunctions.cpp
@@ -28,3 +28,17 @@ void copyFile(string name, string origin, string destination) {
 	if (errorValue)
 		throw "Error in Copying the File";
 }
+
+void generateMetadata(vector<Folder> folders, string destination) {
+	fstream metaDataFile(destination + '\\' + "metadata.txt", fstream::out);
+
+	for (auto folder : folders) {
+		metaDataFile << "|Folder: " << folder.name << '\n'
+			<< '|' << string(2, '-') << "Content: (" << folder.files.size() << " Audio Files, Total Duration: "
+			<< folder.getTotalDuration() << " Seconds)\n";
+		for (auto file : folder.files) {
+			metaDataFile << '|' << string(4, '-') << "File: " << file.name << '\t' << "Duration: " << file.duartion << " Seconds \n";
+		}
+		metaDataFile << "\n\n";
+	}
+}
--- SoundPacker/HelperFunctions.h
+++ SoundPacker/HelperFunctions.h
@@ -8,4 +8,4 @@ using namespace std;
 bool directoryExists(string directory);
 void createFolder(string name, string path);
 void copyFile(string name, string origin, string destination);
-void generateMetaData(vector<Folder> folders, string destination);
+void generateMetadata(vector<Folder> folders, string destination);
