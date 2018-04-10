f9eb58f9ea1a63b8ed7f259c7d2eebca74fb7547
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2018-04-10 12:26:05 +0200

    Implemented createFolder function

--- SoundPacker/HelperFunctions.cpp
+++ SoundPacker/HelperFunctions.cpp
@@ -1,1 +1,20 @@
 #include "HelperFunctions.h"
+
+bool directoryExists(string directory) {
+	DWORD attributes = GetFileAttributesA(directory.c_str());
+
+	if (attributes == INVALID_FILE_ATTRIBUTES)
+		return false;
+
+	return (attributes & FILE_ATTRIBUTE_DIRECTORY);
+}
+
+void createFolder(string name, string path) {
+	string folderDirectory = path + '\\' + name;
+	if (!directoryExists(folderDirectory)) {
+		string command = "mkdir " + folderDirectory;
+		bool errorValue = system(command.c_str());
+		if (errorValue)
+			throw "Error in Creating the Folder";
+	}
+}
