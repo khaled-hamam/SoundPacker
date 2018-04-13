e0333053fbc70bbf1060496b261d18e72b9ec540
karim <karim.abdelghaffar98@gmail.com>
2018-04-13 20:12:25 +0200

    Worst fit decreasing ( linear & PQ ) & first Fit decreasing & folder Filling Algorithms

--- SoundPacker/SoundPackingLib.cpp
+++ SoundPacker/SoundPackingLib.cpp
@@ -30,6 +30,8 @@ const bool operator>(const Folder &a, const Folder &b) {
 	return a.totalDuration > b.totalDuration;
 }
 
+bool compareFunction(File i, File j) { return (i.duration < j.duration); }
+
 std::vector<Folder> WorstFitLS(std::vector<File> files, int MaxDuration) {
 	vector<Folder>folders;
 
@@ -87,3 +89,127 @@ std::vector<Folder> WorstFitPQ(std::vector<File> files, int MaxDuration) {
 
 	return folder;
 }
+
+vector<Folder> worstFitDecreasingLS(vector<File> inputFiles, float maxDuration)
+{
+	sort(inputFiles.rbegin(), inputFiles.rend(), compareFunction);
+	vector<Folder> myFolders;
+	Folder fisrtFolder;
+	fisrtFolder.totalDuration = 0;
+	myFolders.push_back(fisrtFolder);
+	for (int i = 0; i < inputFiles.size(); i++) {
+		int wstidx = -1;
+		int max = -1;
+		for (int j = 0; j < myFolders.size(); j++) {
+			if (maxDuration - myFolders[j].totalDuration >= inputFiles[i].duration) {
+				if (maxDuration - myFolders[j].totalDuration > max) {
+					max = maxDuration - myFolders[j].totalDuration;
+					wstidx = j;
+				}
+			}
+		}
+		if (wstidx != -1) {
+			myFolders[wstidx].files.push_back(inputFiles[i]);
+			myFolders[wstidx].totalDuration += inputFiles[i].duration;
+		}
+		else {
+			Folder newFolder;
+			newFolder.totalDuration = inputFiles[i].duration;
+			myFolders.push_back(newFolder);
+		}
+	}
+	return myFolders;
+}
+
+vector<Folder> worstFitDecreasingPQ(vector<File> inputFiles, float maxDuration)
+{
+	priority_queue < Folder, vector<Folder>, greater<Folder> > myFolderspq;
+	sort(inputFiles.rbegin(), inputFiles.rend(), compareFunction); // O(N Log N)
+	Folder fisrtFolder;
+	fisrtFolder.totalDuration = 0;
+	myFolderspq.push(fisrtFolder); // O(Log M)
+	Folder temp;
+	for (int i = 0; i < inputFiles.size(); i++) {
+		if (inputFiles[i].duration <= (maxDuration - myFolderspq.top().totalDuration)) {
+			temp = myFolderspq.top(); //O(1)
+			temp.files.push_back(inputFiles[i]); // O(1)
+			temp.totalDuration += inputFiles[i].duration; // O(1)
+			myFolderspq.pop(); // O(log M)
+			myFolderspq.push(temp); // O(Log M)
+		}
+		else {
+			temp.totalDuration = inputFiles[i].duration;;
+			temp.files.push_back(inputFiles[i]); // O(1)
+			myFolderspq.push(temp); //O(Log M)
+		}
+	}
+	vector<Folder> myFolders;
+	for (int i = 0; i < myFolderspq.size(); i++) {
+		myFolders.push_back(myFolderspq.top()); // O(1)
+		myFolderspq.pop(); // O(log M)
+	}
+	return myFolders;
+}
+
+vector<Folder> FirstFitDecreasingLS(vector<File> inputFiles, float maxDuration)
+{
+	vector<Folder> myFolders;
+	for (int i = 0; i < inputFiles.size(); i++) {
+		int k = 0;
+		for (int j = 0; j < myFolders.size(); j++) {
+			if ((maxDuration - myFolders[j].totalDuration) >= inputFiles[i].duration) {
+				myFolders[j].files.push_back(inputFiles[i]);
+				myFolders[j].totalDuration += inputFiles[i].duration;
+				k = 1;
+				break;
+			}
+		}
+		if (!k) {
+			Folder folder;
+			folder.files.push_back(inputFiles[i]);
+			folder.totalDuration = inputFiles[i].duration;
+			myFolders.push_back(folder);
+		}
+	}
+	return myFolders;
+}
+int dp[10001][351];
+bool store[10001][351];
+vector<Folder> folderFilling(vector<File> files, float maxDuration)
+{
+	vector<Folder> folders;
+	while (!files.empty()) {
+		memset(dp, -1, sizeof dp);
+		memset(store, 0, sizeof store);
+		Folder folder;
+		for (int i = 0; i <= files.size(); i++) {
+			for (int j = 0; j <= maxDuration; j++) {
+				int duration = files[i - 1].duration;
+				if (!i) {
+					dp[i][j] = 0;
+				}
+				else if (duration <= j && dp[i - 1][j] < duration + dp[i - 1][j - duration]) {
+					dp[i][j] = duration + dp[i - 1][j - duration];
+					store[i][j] = true;
+				}
+				else {
+					dp[i][j] = dp[i - 1][j];
+					store[i][j] = false;
+				}
+			}
+		}
+		cout << dp[files.size()][(int)maxDuration] << endl;
+		int j = maxDuration;
+		for (int i = files.size(); i >= 1; i--) {
+			if (store[i][j]) {
+				folder.files.push_back(files[i - 1]);
+				j -= files[i - 1].duration;
+				files.erase(files.begin() + (i - 1));
+			}
+		}
+
+		folders.push_back(folder);
+	}
+	return folders;
+}
+
--- SoundPacker/SoundPackingLib.h
+++ SoundPacker/SoundPackingLib.h
@@ -35,17 +35,21 @@ public:
 
 
 
-const std::string WORST_FIT_LS		= "Worst Fit Linear Seach";
-const std::string WORST_FIT_PQ		= "Worst Fit Priority Queue";
-const std::string WORST_FIT_DEC_LS	= "Worst Fit Dec. Linear Search";
-const std::string WORST_FIT_DEC_PQ	= "Worst Fit Dec. Priority Queue";
-const std::string FIRST_FIT			= "First Fit";
-const std::string BEST_FIT			= "Best Fit";
-const std::string FOLDER_FILLING	= "Folder Filling";
-const std::string MULTITHREADING	= "Multithreading Algorithm";
+const std::string WORST_FIT_LS = "Worst Fit Linear Seach";
+const std::string WORST_FIT_PQ = "Worst Fit Priority Queue";
+const std::string WORST_FIT_DEC_LS = "Worst Fit Dec. Linear Search";
+const std::string WORST_FIT_DEC_PQ = "Worst Fit Dec. Priority Queue";
+const std::string FIRST_FIT = "First Fit";
+const std::string BEST_FIT = "Best Fit";
+const std::string FOLDER_FILLING = "Folder Filling";
+const std::string MULTITHREADING = "Multithreading Algorithm";
 
 // TODO: Put runAlgorithm in HelperFunctions
 double runAlgorithm(std::string inputFile, std::string algorithm, float duration, bool allowCopy);
 
 std::vector<Folder> WorstFitLS(std::vector<File> files, int MaxDuration);
 std::vector<Folder> WorstFitPQ(std::vector<File> files, int MaxDuration);
+std::vector<Folder> worstFitDecreasingLS(std::vector<File> inputFiles, float maxDuration);
+std::vector<Folder> worstFitDecreasingPQ(std::vector<File> inputFiles, float maxDuration);
+std::vector<Folder> FirstFitDecreasingLS(std::vector<File> inputFiles, float maxDuration);
+std::vector<Folder> folderFilling(std::vector<File> files, float maxDuration);
